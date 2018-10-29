02177a94214623d25e9715c02bc7893fa1148622
Tarc <Tarc@B90>
2018-10-29 11:22:11 +0800

    My Second Sample

--- src/mynewgui/MyNewGUI.java
+++ src/mynewgui/MyNewGUI.java
@@ -17,6 +17,10 @@ public class MyNewGUI {
     public static void main(String[] args) {
         
         System.out.println("Hello world !!");
+        
+        System.out.println("Hello world 2 !!");
+        
+        System.out.println("Hello world war !!");
         // TODO code application logic here
     }
     
